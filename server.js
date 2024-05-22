73e818f2134a90fea6a416fd4442147345f45732
AngelDel <44862180+AngelDel@users.noreply.github.com>
2024-05-22 12:38:47 +0100

    Update server.js - add console log

--- server.js
+++ server.js
@@ -12,6 +12,8 @@ app.get('/', (req, res) => {
 // Define the port for the server to listen on
 const PORT = process.env.PORT || 3000;
 
+console.log(`Hello from the server`);
+
 // Start the server
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
